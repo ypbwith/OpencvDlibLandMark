3148195397d2626f6325fd7bda6c50f7066378a4
ypbwith <yangpengbo2636@gmail.com>
2017-08-04 16:00:01 +0800

    Add files via upload

--- SpeedDlibLandMark_Track.cpp
+++ SpeedDlibLandMark_Track.cpp
@@ -7,14 +7,41 @@
 #include <dlib/gui_widgets.h>  
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include "Python.h"
+#include <math.h>
 
 using namespace dlib;
 using namespace std;
 using namespace cv;
 
 #define RATIO 1
-#define SKIP_FRAMES 300
+#define SKIP_FRAMES 100
+#define AlarmLevel 0.05
+#define AlarmCount 15
+int alarmCount = 0;
+double eyesClosedLevel;
 
+struct leftEyePoint 
+{
+	double x[6];
+	double y[6];
+};
+
+struct rightEyePoint
+{
+	double x[6];
+	double y[6];
+};
+
+struct eyeKeyPoint 
+{
+	leftEyePoint leftEye;
+
+	rightEyePoint rightEye;
+
+};
+
+eyeKeyPoint eyePoint;
 
 void draw_polyline(cv::Mat &img, const dlib::full_object_detection& d, const int start, const int end, bool isClosed = false)
 {
@@ -23,7 +50,8 @@ void draw_polyline(cv::Mat &img, const dlib::full_object_detection& d, const int
 	{
 		points.push_back(cv::Point(d.part(i).x(), d.part(i).y()));
 	}
-	cv::polylines(img, points, isClosed, cv::Scalar(255, 0, 0), 2, 16);
+
+	cv::polylines(img, points, isClosed, cv::Scalar(255, 0, 0), 1, 8);
 
 }
 
@@ -46,6 +74,8 @@ void render_face(cv::Mat &img, const dlib::full_object_detection& d)
 	draw_polyline(img, d, 48, 59, true);    // Outer lip
 	draw_polyline(img, d, 60, 67, true);    // Inner lip
 
+	
+
 }
 
 void ZeroRect(dlib::rectangle &rect)
@@ -55,11 +85,36 @@ void ZeroRect(dlib::rectangle &rect)
 	rect.set_left(0);
 	rect.set_right(0);
 }
-int main()
+
+int main(int argc, char* argv[])
 {
+	Py_Initialize();//初始化python  
+	
+	// 检查初始化是否成功  
+	if (!Py_IsInitialized()) {
+		return -1;
+	}
+
+	PyObject *pModule = NULL, *pFunc = NULL, *pArg = NULL;
+
+	pModule = PyImport_ImportModule("pysound");//引入模块  
+	
+	if (!pModule) {
+		printf("can't find pysound.py");
+		getchar();
+		return -1;
+	}
+
+	pFunc = PyObject_GetAttrString(pModule, "sound_alarm");//直接获取模块中的函数  
+
+	//pArg = Py_BuildValue("(s)", "alarm.wav"); //参数类型转换，传递一个字符串。将c/c++类型的字符串转换为python类型，元组中的python类型查看python文档  
+
+	//PyEval_CallObject(pFunc, pArg); //调用直接获得的函数，并传递参数  
+
+	//Py_Finalize(); //释放python  
+
 	try
 	{
-		//cv::VideoCapture cap("The Great Dictator.mp4");
 		cv::VideoCapture cap(0);
 		//image_window win;
 		//cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);  
@@ -79,13 +134,12 @@ int main()
 		//while (!win.is_closed())
 		// Grab a frame  
 		cv::Mat img, img_small;
-		
-
 
 		// Define initial boundibg box
 		Rect2d facebox(0, 0, 0, 0);
 		Rect2d facebox_RATIO(0, 0, 0, 0);
 		dlib::rectangle facePostion(0,0,0,0);
+
 		while (1)
 		{
 			cap >> img;
@@ -110,10 +164,7 @@ int main()
 					facebox_RATIO.width = faces.at(0).right() - facebox_RATIO.x;
 					facebox_RATIO.height = faces.at(0).bottom() - facebox_RATIO.y;
 
-		
 					tracker.start_track(cimg_small, centered_rect(point(facebox_RATIO.x + 0.5*facebox_RATIO.width, facebox_RATIO.y + 0.5*facebox_RATIO.height), facebox_RATIO.width, facebox_RATIO.height));//centered_rect(point(93, 110), 38, 86));
-
-					//printf("----------------------what  1----------------------------\n");
 
 					trackflag = 1;
 				}
@@ -148,12 +199,12 @@ int main()
 					(long)(facePostion.bottom() * RATIO)
 				);
 
-				//cout<< (long)(faces[1].left())<<'\n';
-				facebox.x = r.left();
-				facebox.y = r.top();
-				facebox.width = r.right() - facebox.x;
-				facebox.height = r.bottom() - facebox.y;
-				cv::rectangle(img, facebox, Scalar(255, 0, 0), 2, 1);
+				////cout<< (long)(faces[1].left())<<'\n';
+				//facebox.x = r.left();
+				//facebox.y = r.top();
+				//facebox.width = r.right() - facebox.x;
+				//facebox.height = r.bottom() - facebox.y;
+				//cv::rectangle(img, facebox, Scalar(255, 0, 0), 2, 1);
 
 
 				// Landmark detection on full sized image
@@ -162,6 +213,61 @@ int main()
 
 				// Custom Face Render
 				render_face(img, shape);
+
+				for (int i = 36; i <= 41; i++)
+				{
+
+					eyePoint.leftEye.x[i - 36] = shape.part(i).x();
+					eyePoint.leftEye.y[i - 36] = shape.part(i).y();
+	            }
+
+				for (int i = 42; i <= 47; i++)
+				{
+
+					eyePoint.leftEye.x[i - 42] = shape.part(i).x();
+					eyePoint.leftEye.y[i - 42] = shape.part(i).y();
+				}
+
+				eyesClosedLevel =
+					   (
+					   ((pow(eyePoint.leftEye.x[1] - eyePoint.leftEye.x[5], 2) + pow(eyePoint.leftEye.y[1] - eyePoint.leftEye.y[5], 2)) +
+						(pow(eyePoint.leftEye.x[2] - eyePoint.leftEye.x[4], 2) + pow(eyePoint.leftEye.y[2] - eyePoint.leftEye.y[4], 2))
+						)
+						/ (2 * (pow(eyePoint.leftEye.x[0] - eyePoint.leftEye.x[3], 2) + pow(eyePoint.leftEye.y[0] - eyePoint.leftEye.y[3], 2)))
+
+						+
+
+						((pow(eyePoint.leftEye.x[1] - eyePoint.leftEye.x[5], 2) + pow(eyePoint.leftEye.y[1] - eyePoint.leftEye.y[5], 2)) +
+						(pow(eyePoint.leftEye.x[2] - eyePoint.leftEye.x[4], 2) + pow(eyePoint.leftEye.y[2] - eyePoint.leftEye.y[4], 2))
+							)
+						/ (2 * (pow(eyePoint.leftEye.x[0] - eyePoint.leftEye.x[3], 2) + pow(eyePoint.leftEye.y[0] - eyePoint.leftEye.y[3], 2)))
+
+						) / 2;
+
+				
+				if (eyesClosedLevel < AlarmLevel)
+				{
+                    alarmCount++;
+					if (alarmCount > AlarmCount)
+					{
+							alarmCount = 0;
+
+							pArg = Py_BuildValue("(s)", "alarm.wav"); //参数类型转换，传递一个字符串。将c/c++类型的字符串转换为python类型，元组中的python类型查看python文档  
+
+							PyEval_CallObject(pFunc, pArg); //调用直接获得的函数，并传递参数  
+					}
+				
+				}
+				else
+				{
+					alarmCount = 0;
+				}
+
+				char PutString[20] ;
+				sprintf(PutString, "eyeCloseLevel:%f\n", eyesClosedLevel);
+				printf(PutString);
+				cv::putText(img, PutString, cv::Point(15, 15),
+					FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
 
 			}
 		    
@@ -197,4 +303,9 @@ int main()
 		cout << e.what() << endl;
 	}
 	system("pause");
+
+	Py_Finalize(); //释放python  
+
+	return 0;
+
 }
